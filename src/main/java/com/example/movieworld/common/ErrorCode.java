de0f9bd2636b758ba890f864ddf2660b0b491e1c
littlecold4 <minu09252@gmail.com>
2025-08-05 18:03:27 +0900

    #1 - test : getMovieList Service 실패 경우(페이지 넘버오류)

--- src/main/java/com/example/movieworld/common/ErrorCode.java
+++ src/main/java/com/example/movieworld/common/ErrorCode.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-    ;
+    INVALID_PAGE_NUMBER(HttpStatus.BAD_REQUEST,"잘못된 페이지 넘버입니다.");
     private final HttpStatus httpStatus;
     private final String message;
 
--- src/test/java/com/example/movieworld/getMovieListServiceTest.java
+++ src/test/java/com/example/movieworld/getMovieListServiceTest.java
@@ -1,12 +1,11 @@
 package com.example.movieworld;
 
+import com.example.movieworld.common.CustomException;
+import com.example.movieworld.common.ErrorCode;
 import com.example.movieworld.movie.dto.MovieResDto;
 import com.example.movieworld.movie.repository.MovieRepository;
 import com.example.movieworld.movie.service.MovieService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -18,8 +17,7 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 //@SpringBootTest
@@ -50,22 +48,34 @@ public class getMovieListServiceTest {
 
         expectedResult =  new PageImpl<>(Arrays.asList(testMovie1,testMovie2),pageable,2);
 
-        int pageNum = 0;
-
-        when(movieRepository.getMovieList(0)).thenReturn(expectedResult);
+        when(movieRepository.getMovieList(anyInt())).thenReturn(expectedResult);
     }
 
     @Nested
     @DisplayName("Service _ Movie 리스트 조회 _ 성공")
-    public class success{
+    public class Success{
         @Test
-        @DisplayName("Success _ getMovieList")
+        @DisplayName("성공")
         void success_getMovieList(){
             Page<MovieResDto> actualResult = movieService.getMovieList(0);
 
             assertNotNull(actualResult);
             assertEquals(expectedResult,actualResult);
             verify(movieRepository,times(1)).getMovieList(0);
+        }
+    }
+    @Nested
+    @DisplayName("Service _ Movie 리스트 조회  _ 실패")
+    public class Failure{
+        @Test
+        @DisplayName("실패 _ 페이지 넘버 오류")
+        void fail_Invalid_Page_Number(){
+            int invalidPageNum =-1;
+
+            Exception ex = assertThrows(CustomException.class,
+                    ()-> movieService.getMovieList(invalidPageNum));
+            assertEquals(ErrorCode.INVALID_PAGE_NUMBER.getMessage(),
+                    ex.getMessage());
         }
     }
 }
