8dc4799fccd69ec39732544ede52255dd2397810
littlecold4 <minu09252@gmail.com>
2025-08-05 17:29:50 +0900

    #1 - feat : getMovieList Controller 코드 작성

--- src/main/java/com/example/movieworld/movie/controller/MovieController.java
+++ src/main/java/com/example/movieworld/movie/controller/MovieController.java
@@ -1,9 +1,25 @@
 package com.example.movieworld.movie.controller;
 
+import com.example.movieworld.movie.dto.MovieResDto;
+import com.example.movieworld.movie.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/movie")
 public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping("")
+    public ResponseEntity<Page<MovieResDto>> getMovieList(@RequestParam int pageNum) {
+        return new ResponseEntity<>(movieService.getMovieList(pageNum),HttpStatus.OK);
+    }
+
 }
