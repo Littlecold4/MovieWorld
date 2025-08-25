373f6851c7d439c7b29392562f800e6e58ade2ec
littlecold4 <minu09252@gmail.com>
2025-08-25 17:51:52 +0900

    #8 - feat : unlikeMovie Controller

--- src/main/java/com/example/movieworld/like/controller/LikeController.java
+++ src/main/java/com/example/movieworld/like/controller/LikeController.java
@@ -5,10 +5,7 @@ import com.example.movieworld.user.domain.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/like")
@@ -18,9 +15,15 @@ public class LikeController {
     @PostMapping("/{movieId}")
     public ResponseEntity<String> likeMovie(@AuthenticationPrincipal UserDetailsImpl userDetail,
                                             @PathVariable Long movieId){
-        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
         likeService.likeMovie(movieId,userDetail.getUser().getId());
         return ResponseEntity.ok("좋아요 처리에 성공하였습니다.");
+    }
+
+    @DeleteMapping("/unlike/{movieId}")
+    public ResponseEntity<String> unlikeMovie(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                              @PathVariable Long movieId){
+        likeService.unlikeMovie(movieId,userDetails.getUser().getId());
+        return ResponseEntity.ok("좋아요 취소 처리에 성공하였습니다.");
     }
 
 }
